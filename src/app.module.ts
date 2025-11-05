fc9375eb651f232b6f58dfc361afbb92c0860004
sofiane tigrine <sofiane.tigrine15@gmail.com>
2025-11-05 15:46:58 +0100

    Faute de syntaxe

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PositionModule } from './position/position.module';
-import { PositionModule } from './position/position.module';
 
 @Module({
   imports: [PositionModule],
